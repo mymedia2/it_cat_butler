6765ba1a5cf33bff8de52a5e8f6fd9aaef0f62c7
Nicholas Guriev <guriev-ns@ya.ru>
2016-11-05 11:58:38 +0300

    Regain my code
    * Uncomment auto-detect a language for a group
    * Preview of guide how to make a bot an admin
    * Trap for unknown media type

--- bot.lua
+++ bot.lua
@@ -192,10 +192,10 @@ local function on_msg_receive(msg, callback) -- The fn run whenever a message is
 	else
 		if msg.group_chat_created or (msg.new_chat_member and msg.new_chat_member.id == bot.id) then
 			-- set the language
-			--[[locale.language = db:get(string.format('lang:%d', msg.from.id)) or 'en'
+			locale.language = db:get(string.format('lang:%d', msg.from.id)) or 'en'
 			if not config.available_languages[locale.language] then
 				locale.language = 'en'
-			end]]
+			end
 			
 			-- send disclamer
 			api.sendMessage(msg.chat.id, _([[
@@ -303,6 +303,7 @@ local function parseMessageFunction(update)
 			msg.text = '###migrate_from_chat_id'
 	else
 			--callback = 'onUnknownType'
+			vardump(update)
 			print('Unknown update type') return
 	end
 	
@@ -374,6 +375,7 @@ local function parseMessageFunction(update)
 		function_key = 'onCallbackQuery'
 	else
 		--function_key = 'onUnknownType'
+		vardump(update)
 		print('Unknown update type') return
 	end
 	
